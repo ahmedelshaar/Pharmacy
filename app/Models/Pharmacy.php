ce87f4b4ae7d86b48bb566f2b36de34f2fac0b08
LamisTarek <lameeswaheed99@gmail.com>
2023-04-23 21:27:53 +0200

    pharmacy crud

--- app/Models/Pharmacy.php
+++ app/Models/Pharmacy.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Database\Eloquent\SoftDeletes;
 
 class Pharmacy extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -20,5 +22,7 @@ class Pharmacy extends Model
     {
         return $this->belongsTo(Area::class);
     }
+
+
 
 }
